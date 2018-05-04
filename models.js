0b8ecc7eaca5ff5415aa929293e49d05f784f967
daSmith1105 <mustangdas@gmail.com>
2018-05-03 23:53:33 -0500

    one and done

---
+++ models.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+// this is our schema to represent a restaurant
+const postSchema = mongoose.Schema({
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    author: {
+        firstName: { type: String, required: true },
+        lastName: { type: String, required: true }
+    }
+});
+
+// *virtuals* (http://mongoosejs.com/docs/guide.html#virtuals)
+// allow us to define properties on our object that manipulate
+// properties that are stored in the database. Here we use it
+// to generate a human readable string based on the address object
+// we're storing in Mongo.
+postSchema.virtual('authorName').get(function() {
+    return `${this.author.firstName} ${this.author.lastName}`.trim()
+});
+
+// this is an *instance method* which will be available on all instances
+// of the model. This method will be used to return an object that only
+// exposes *some* of the fields we want from the underlying data
+postSchema.methods.serialize = function() {
+
+    return {
+        id: this._id,
+        title: this.title,
+        content: this.cuisine,
+        author: this.authorName
+    };
+}
+
+// note that all instance methods and virtual properties on our
+// schema must be defined *before* we make the call to `.model`.
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = { Post };
